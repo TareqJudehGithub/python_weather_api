fff5a9c8084b524ef5852096ad4b6ef5d3447db9
Tareq Judeh <tareq.judeh@outlook.com>
2021-01-10 18:44:50 +0200

    Added sensitive data to Python ENV.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import requests
+import smtplib
+import os
+from twilio.rest import Client
 
+# Twilio account details:
+account_sid = "ACCOUNT_SID"
+auth_token = os.environ.get("AUTH_TOKEN")
+sms_from = os.environ.get("SMS_FROM")
+sms_to = os.environ.get("SMS_to")
+
+# Email account details:
+my_user = os.environ.get("MY_EMAIL")
+my_pass = os.environ.get("MY_PASS")
+
+# Open Weather account details:
+api_key = os.environ.get("OMW_API_KEY")
 parameters = {
-    "appid": "e101304296b777bf49aa00d70e2301da",
-    "lat": "31.945368",
-    "lon": "35.928371",
-    "exclude": "current, minutely, daily"
+    "appid": api_key,
+    "lat": "55.755825",
+    "lon": "37.617298",
+    "exclude": "current, minutely, hourly"
 }
 response = requests.get(
     url="https://api.openweathermap.org/data/2.5/onecall",
     params=parameters
 )
+
 print("\n")
+
 print(f"Response: {response.status_code}")
 response.raise_for_status()
 weather_data = response.json()
@@ -19,7 +36,7 @@ weather_data = response.json()
 # weather condition codes is less than 700
 
 # TODO weather forecast for only the next 12 hours:
-weather_slice = weather_data["hourly"][:12]
+weather_slice = weather_data["daily"][:1]
 
 # Loop through every id in weather list:
 will_rain = False
@@ -31,13 +48,32 @@ for hour_data in weather_slice:
         will_rain = True
 
     if will_rain:
+        # Send SMS:
+        client = Client(account_sid, auth_token)
+        message = client.messages \
+            .create(
+            body="Its going to rain now!",
+            from_=os.environ.get("SMS_FROM"),
+            to=os.environ.get("SMS_to")
+        )
+        # Check if sms was successfully sent:
+        print(message.status)
+        print("\n")
+
+        # Send Email:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(
+                user=my_user,
+                password=my_pass
+            )
+            connection.sendmail(
+                from_addr=my_user,
+                to_addrs="tareq.joudeh@gmail.com",
+                msg="Subject: It's going to rain!\n\n Rainy day today! let it rain!\n Tareq Pythoneer".encode("utf8")
+            )
         print("Bring an umbrella")
     else:
-        print("Today it's sunny!")
+        print("Sunny day today!")
 
-
-
-
-
-
-
+ 
